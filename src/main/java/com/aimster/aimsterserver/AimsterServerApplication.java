bc71717c8add24b1c85bcaf06c5e026314b5505e
stephengeorge568 <70499850+stephengeorge568@users.noreply.github.com>
2022-08-18 16:33:17 -0500

    Spring init

---
+++ src/main/java/com/aimster/aimsterserver/AimsterServerApplication.java
@@ -0,0 +1,13 @@
+package com.aimster.aimsterserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimsterServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AimsterServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aimster/aimsterserver/AimsterServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aimster.aimsterserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AimsterServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
